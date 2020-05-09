8afff8b8ab264725c30dcac8f9640f11e9a757c9
razi-ed <razi.94@outlook.com>
2020-05-09 16:24:52 +0530

    adds cleaning up to build system

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 module.exports = {
   entry: { main: './src/index.js' },
@@ -25,6 +27,9 @@ module.exports = {
     ]
   },
   plugins: [
+    new CleanWebpackPlugin({
+      cleanAfterEveryBuildPatterns: ['dist']
+    }),
     new MiniCssExtractPlugin({
       filename: 'styles.css',
     }),
