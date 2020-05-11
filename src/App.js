e6a771ece425ac3a090cf6c9f969a6721a201329
razi-ed <razi.94@outlook.com>
2020-05-11 23:51:39 +0530

    layout changes

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   };
   return (
     <Layout>
-      <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
           <Menu.Item key="1" icon={<UserOutlined />}>
@@ -35,7 +35,7 @@ const App = () => {
       <Layout className="site-layout">
         <Header className="site-layout-background" style={{ padding: 0 }}>
           {React.createElement(
-            this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
             {
               className: 'trigger',
               onClick: toggle,
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import App from './App';
 
+import '../node_modules/antd/dist/antd.css';
 import './index.css';
 
-const App = () => {
-  return <div>{'Hello world!'}</div>;
-};
+// const App = () => {
+//   return <div>{'Hello world!'}</div>;
+// };
 
 render(<App />, document.getElementById('app'));
